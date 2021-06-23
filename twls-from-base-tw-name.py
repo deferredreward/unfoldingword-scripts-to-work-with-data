933e91c7c99589901dfdf8a9e530ecfadacc7bed
Benjamin Wright <benjamin.wright@unfoldingword.org>
2021-06-23 11:05:38 -0400

    new twl script

--- twls-from-base-tw-name.py
+++ twls-from-base-tw-name.py
@@ -1,18 +1,19 @@
 import io
 
 # CHANGE THIS to the path to your TWL file, it might be in your downloads directory
-filename = "C:/Users/benja/Google Drive/scripting/twl_EXO.tsv"
+filename = "C:/Users/benja/Google Drive/scripting/twl_EXO - sample.tsv"
 
 twbaselink = "rc://*/tw/dict/bible"
 
 with io.open("twlinksindex.txt", encoding='utf8') as f:
     twlinkslist = f.readlines()
 
-
 with io.open(filename, encoding='utf8') as f:
     with io.open(filename.replace('.tsv','-new.tsv'), 'w', encoding='utf8', newline='\n') as newFile:
         for line in f:
+
             ls = line.split('\t')
+            if '\n' not in ls[5]: ls[5] += '\n'
             if twbaselink + "/kt/" + ls[5] in twlinkslist:
                 ls[2] = "keyterm"
                 ls[5] = twbaselink + "/kt/" + ls[5]
