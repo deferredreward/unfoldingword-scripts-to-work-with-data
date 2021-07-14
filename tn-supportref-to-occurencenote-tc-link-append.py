af3a613a1647414962b50473a9f058a0a79600e2
Benjamin Wright <benjamin.wright@unfoldingword.org>
2021-07-14 17:39:40 -0400

    auto file renaming

--- tn-supportref-to-occurencenote-tc-link-append.py
+++ tn-supportref-to-occurencenote-tc-link-append.py
@@ -1,10 +1,12 @@
-import io
+import io, os
 
 # CHANGE THIS to the path to your TN file, it might be in your downloads directory
-filename = "C:/Users/benja/Google Drive/scripting/en_tn_02-EXO.tsv"
+filename = "C:/Users/benja/Documents/uwgit/en_tn/en_tn_02-EXO.tsv"
+os.rename(filename,filename.replace('.tsv','.old'))
+filename = filename.replace('.tsv','.old')
 
 with io.open(filename, encoding='utf8') as f:
-    with io.open(filename.replace('.tsv','-new.tsv'), 'w', encoding='utf8', newline='\n') as newFile:
+    with io.open(filename.replace('.old','.tsv'), 'w', encoding='utf8', newline='\n') as newFile:
         for line in f:
             ls = line.split('\t')
             if "-" in ls[4] and "[[rc:" not in ls[8]:
--- twls-from-base-tw-name.py
+++ twls-from-base-tw-name.py
@@ -1,18 +1,21 @@
-import io
+import io, os
 
 # CHANGE THIS to the path to your TWL file, it might be in your downloads directory
-filename = "C:/Users/benja/Google Drive/scripting/twl_EXO - sample.tsv"
-
+filename = "C:/Users/benja/Documents/uwgit/en_twl/twl_EXO.tsv"
+os.rename(filename,filename.replace('.tsv','.old'))
+filename = filename.replace('.tsv','.old')
 twbaselink = "rc://*/tw/dict/bible"
 
 with io.open("twlinksindex.txt", encoding='utf8') as f:
     twlinkslist = f.readlines()
 
 with io.open(filename, encoding='utf8') as f:
-    with io.open(filename.replace('.tsv','-new.tsv'), 'w', encoding='utf8', newline='\n') as newFile:
+    with io.open(filename.replace('.old','.tsv'), 'w', encoding='utf8', newline='\n') as newFile:
         for line in f:
 
             ls = line.split('\t')
+            # ls[5] is the TWLink field
+            # ls[2] is the tag field
             if '\n' not in ls[5]: ls[5] += '\n'
             if twbaselink + "/kt/" + ls[5] in twlinkslist:
                 ls[2] = "keyterm"
