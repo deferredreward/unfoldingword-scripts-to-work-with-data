265e81f800484b6f495d876abae4cd3b048c9056
Benjamin Wright <benjamin.wright@unfoldingword.org>
2021-08-10 15:19:36 -0400

    Update resequence-hebrew.py

--- resequence-hebrew.py
+++ resequence-hebrew.py
@@ -3,13 +3,14 @@ import io
 # CHANGE THIS to the path to your file, it might be in your downloads directory
 filename = "C:/Users/benja/Downloads/05-DEU-master.usfm"
 outputfile = "C:/Users/benja/Downloads/05-DEU.usfm"
-
+print("starting")
 with io.open(filename, encoding='utf8') as f:
     with io.open(outputfile, 'w', encoding='utf8', newline='\n') as newFile:
-
+        print("files open")
         content = f.read()
         # daghesh issues
         # all items are swapping the named item for daghesh as in first
+        content = content.replace('\u05BD\u05BC',u'\u05BC\u05BD') # meteg
         content = content.replace('\u05B1\u05BC',u'\u05BC\u05B1') # hatheph seghol-daghesh => daghesh-hatheph seghol
         content = content.replace('\u05B2\u05BC',u'\u05BC\u05B2') # hateph pathach
         content = content.replace('\u05B3\u05BC',u'\u05BC\u05B3') # hateph qamets
@@ -18,13 +19,14 @@ with io.open(filename, encoding='utf8') as f:
         content = content.replace('\u05B6\u05BC',u'\u05BC\u05B6') # seghol
         content = content.replace('\u05B7\u05BC',u'\u05BC\u05B7') # pathach
         content = content.replace('\u05B8\u05BC',u'\u05BC\u05B8') # qamets
+        content = content.replace('\u05C7\u05BC',u'\u05BC\u05C7') # qamets qatan
         content = content.replace('\u05B9\u05BC',u'\u05BC\u05B9') # holem
         content = content.replace('\u05BA\u05BC',u'\u05BC\u05BA') # waw holem
         content = content.replace('\u05BB\u05BC',u'\u05BC\u05BB') # qibbuts
-        content = content.replace('\u05BD\u05BC',u'\u05BC\u05BD') # meteg
         content = content.replace('\u05B0\u05BC',u'\u05BC\u05B0') # shewa
 
         # shin dot issues, same sequence as above
+        content = content.replace('\u05BD\u05C1',u'\u05C1\u05BD') # meteg
         content = content.replace('\u05B1\u05C1',u'\u05C1\u05B1') # hatheph seghol
         content = content.replace('\u05B2\u05C1',u'\u05C1\u05B2') # hateph pathach
         content = content.replace('\u05B3\u05C1',u'\u05C1\u05B3') # hateph qamets
@@ -33,13 +35,14 @@ with io.open(filename, encoding='utf8') as f:
         content = content.replace('\u05B6\u05C1',u'\u05C1\u05B6') # seghol
         content = content.replace('\u05B7\u05C1',u'\u05C1\u05B7') # pathach
         content = content.replace('\u05B8\u05C1',u'\u05C1\u05B8') # qamets
+        content = content.replace('\u05C7\u05C1',u'\u05C1\u05C7') # qamets qatan
         content = content.replace('\u05B9\u05C1',u'\u05C1\u05B9') # holem
         content = content.replace('\u05BA\u05C1',u'\u05C1\u05BA') # waw holem
         content = content.replace('\u05BB\u05C1',u'\u05C1\u05BB') # qibbuts
-        content = content.replace('\u05BD\u05C1',u'\u05C1\u05BD') # meteg
         content = content.replace('\u05B0\u05C1',u'\u05C1\u05B0') # shewa-shin => shin-shewa
 
         # sin dot issues
+        content = content.replace('\u05BD\u05C2',u'\u05C2\u05BD') # meteg
         content = content.replace('\u05B1\u05C2',u'\u05C2\u05B1') # hatheph seghol
         content = content.replace('\u05B2\u05C2',u'\u05C2\u05B2') # hateph pathach
         content = content.replace('\u05B3\u05C2',u'\u05C2\u05B3') # hateph qamets
@@ -48,10 +51,10 @@ with io.open(filename, encoding='utf8') as f:
         content = content.replace('\u05B6\u05C2',u'\u05C2\u05B6') # seghol
         content = content.replace('\u05B7\u05C2',u'\u05C2\u05B7') # pathach
         content = content.replace('\u05B8\u05C2',u'\u05C2\u05B8') # qamets
+        content = content.replace('\u05C7\u05C2',u'\u05C2\u05C7') # qamets qatan
         content = content.replace('\u05B9\u05C2',u'\u05C2\u05B9') # holem
         content = content.replace('\u05BA\u05C2',u'\u05C2\u05BA') # waw holem
         content = content.replace('\u05BB\u05C2',u'\u05C2\u05BB') # qibbuts
-        content = content.replace('\u05BD\u05C2',u'\u05C2\u05BD') # meteg
         content = content.replace('\u05B0\u05C2',u'\u05C2\u05B0') # shewa-sin => sin-shewa
 
         # swap daghesh for shin, sin dot
@@ -59,3 +62,4 @@ with io.open(filename, encoding='utf8') as f:
         content = content.replace('\u05BC\u05C2',u'\u05C2\u05BC') # sin
 
         newFile.write(content) 
+        print("done")
