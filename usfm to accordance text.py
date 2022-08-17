717458e04d84549d4449e780efef3d857da5e080
Benjamin Wright <benjamin.wright@unfoldingword.org>
2022-08-17 09:42:49 -0500

    a script that converts unaligned usfm to txt for import to accordance

---
+++ usfm to accordance text.py
@@ -0,0 +1,79 @@
+import io, re, glob
+
+testingfilepath = "c:/Users/benja/Downloads/ult/"
+testingfilename = "19-PSA.usfm"
+
+filesLocation = "C:/Users/benja/Downloads/ult/*.usfm"
+
+bibleAsList = []
+
+for filename in glob.glob(filesLocation):
+
+    with io.open(filename, encoding='utf8') as f:
+        bookLines = f.readlines()
+
+    chapter = ''
+    paragraph = True
+    book = ''
+    verseNumber = ''
+    newLine = ''
+    newChapter = True
+    title = ''
+    titleLine = False
+
+    for line in bookLines:
+        if line.startswith('\\v'):
+            if not newChapter:
+                bibleAsList.append(newLine)
+                newLine = ''            
+            verseNumber = re.split('\s', line)[1]
+            newLine += verseNumber
+            if len(title) > 0:
+                newLine += title
+                title = ''
+            if paragraph:
+                if not titleLine:
+                    newLine += ' ¶'
+                else: 
+                    newLine += ' <br>'
+                    titleLine = False
+                paragraph = False
+            verseContent = re.split('\s', line, 2)[2]
+            verseContent = verseContent.replace('\\sp', '<br>')
+            verseContent = verseContent.replace('\\qs*', '')
+            verseContent = verseContent.replace('\\qs', ' ')
+            verseContent = verseContent.replace('\\q1', '<br>')
+            verseContent = verseContent.replace('\\q2', '<br>')
+            verseContent = verseContent.replace('\\q', '<br>')
+            newLine += " " + verseContent
+            if newChapter:
+                bibleAsList.append(newLine)
+                newLine = '' 
+                newChapter = False
+                
+        elif line.startswith('\\p') or line.startswith('\\q') or line.startswith(r'\\q\d'):
+            paragraph = True
+
+        elif line.startswith('\\d'): # include Psalm titles
+            title = ' ¶ ' + re.split('\s', line, 1)[1].strip()
+            titleLine = True
+        elif re.split('\s', line)[0] == '\\c':
+            chapter = re.split('\s', line)[1]
+            newChapter = True
+            newLine += chapter + ":"
+        elif line.startswith('\\h'):
+            book = re.split('\s', line, 1)[1]
+            if book.startswith("Song"): book = "Song"
+            if book[0] == '1' or book[0] == '2' or book[0] == '3':
+                book = book[0] + book[2:]
+            newLine += book.strip() + ' '
+    
+
+with io.open('Bible for Accordance.txt', 'w', encoding='utf-8')as f:
+    f.writelines(bibleAsList)
+
+"""
+for item in bookAsList:
+    print(item)
+
+"""
