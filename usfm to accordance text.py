a679776eca88cdf2e581d5e299bea5ea33665add
Benjamin Wright <benjamin.wright@unfoldingword.org>
2022-08-17 14:26:46 -0500

    fix for verse bridges

--- usfm to accordance text.py
+++ usfm to accordance text.py
@@ -29,34 +29,67 @@ def turnUSFMintoTxt(filesLocation, uSLt):
                     bibleAsList.append(newLine)
                     newLine = ''            
                 verseNumber = re.split('\s', line)[1]
-                newLine += verseNumber
-                if len(title) > 0:
-                    newLine += title
-                    title = ''
-                if paragraph:
-                    if not titleLine:
-                        newLine += ' ¶'
-                    else: 
-                        newLine += ' <br>'
-                        titleLine = False
-                    paragraph = False
-                verseContent = re.split('\s', line, 2)[2]
-                verseContent = verseContent.replace('\\sp', '<br>')
-                verseContent = verseContent.replace('\\qs*', '')
-                verseContent = verseContent.replace('\\qs', ' ')
-                verseContent = verseContent.replace('\\q1', '<br>')
-                verseContent = verseContent.replace('\\q2', '<br>')
-                verseContent = verseContent.replace('\\q', '<br>')
-                verseContent = verseContent.replace('\\p', '<br>')
-                verseContent = verseContent.replace('\\li', '<br>')
-                verseContent = verseContent.replace('\\m', '<br>')
-                verseContent = verseContent.replace('\\f +', '<i><b>fn:</b> ')
-                verseContent = verseContent.replace('\\f*', '</i>')               
-                newLine += " " + verseContent
-                if newChapter:
-                    bibleAsList.append(newLine)
-                    newLine = '' 
-                    newChapter = False
+                if verseNumber.find('-') > -1:
+                    verseRange = verseNumber.split('-')
+                    for x in range(int(verseRange[0]), int(verseRange[1]) + 1):
+                        newLine += str(x) + " (" + verseRange[0] + "-" + verseRange[1] + ") "
+                        if len(title) > 0:
+                            newLine += title
+                            title = ''
+                        if paragraph:
+                            if not titleLine:
+                                newLine += ' ¶'
+                            else: 
+                                newLine += ' <br>'
+                                titleLine = False
+                            paragraph = False
+                        verseContent = re.split('\s', line, 2)[2]
+                        verseContent = verseContent.replace('\\sp', '<br>')
+                        verseContent = verseContent.replace('\\qs*', '')
+                        verseContent = verseContent.replace('\\qs', ' ')
+                        verseContent = verseContent.replace('\\q1', '<br>')
+                        verseContent = verseContent.replace('\\q2', '<br>')
+                        verseContent = verseContent.replace('\\q', '<br>')
+                        verseContent = verseContent.replace('\\p', '<br>')
+                        verseContent = verseContent.replace('\\li', '<br>')
+                        verseContent = verseContent.replace('\\m', '<br>')
+                        verseContent = verseContent.replace('\\f +', '<i><b>fn:</b> ')
+                        verseContent = verseContent.replace('\\f*', '</i>')               
+                        newLine += " " + verseContent
+                        if newChapter:
+                            bibleAsList.append(newLine)
+                            newLine = '' 
+                            newChapter = False
+                    
+                else:        
+                    newLine += verseNumber
+                    if len(title) > 0:
+                        newLine += title
+                        title = ''
+                    if paragraph:
+                        if not titleLine:
+                            newLine += ' ¶'
+                        else: 
+                            newLine += ' <br>'
+                            titleLine = False
+                        paragraph = False
+                    verseContent = re.split('\s', line, 2)[2]
+                    verseContent = verseContent.replace('\\sp', '<br>')
+                    verseContent = verseContent.replace('\\qs*', '')
+                    verseContent = verseContent.replace('\\qs', ' ')
+                    verseContent = verseContent.replace('\\q1', '<br>')
+                    verseContent = verseContent.replace('\\q2', '<br>')
+                    verseContent = verseContent.replace('\\q', '<br>')
+                    verseContent = verseContent.replace('\\p', '<br>')
+                    verseContent = verseContent.replace('\\li', '<br>')
+                    verseContent = verseContent.replace('\\m', '<br>')
+                    verseContent = verseContent.replace('\\f +', '<i><b>fn:</b> ')
+                    verseContent = verseContent.replace('\\f*', '</i>')               
+                    newLine += " " + verseContent
+                    if newChapter:
+                        bibleAsList.append(newLine)
+                        newLine = '' 
+                        newChapter = False
                     
             elif line.startswith('\\p') or line.startswith('\\q') or line.startswith(r'\\q\d'):
                 paragraph = True
@@ -76,7 +109,7 @@ def turnUSFMintoTxt(filesLocation, uSLt):
                 newLine += book.strip() + ' '
         
 
-    with io.open("C:/Users/benja/Downloads/" + uSLt + ' for Accordance.txt', 'w', encoding='utf-8')as f:
+    with io.open("C:/Users/benja/Downloads/" + uSLt + ' for Accordance.txt', 'w', encoding='utf-8') as f:
         f.writelines(bibleAsList)
 
 turnUSFMintoTxt("C:/Users/benja/Downloads/ust/*.usfm", "UST")
