2d8d15601f60388b98ae18f369972ad90a11bfe4
Benjamin Wright <benjamin.wright@unfoldingword.org>
2022-08-17 12:25:33 -0500

    Update usfm to accordance text.py

--- usfm to accordance text.py
+++ usfm to accordance text.py
@@ -1,79 +1,83 @@
 import io, re, glob
 
-testingfilepath = "c:/Users/benja/Downloads/ult/"
-testingfilename = "19-PSA.usfm"
+# testingfilepath = "c:/Users/benja/Downloads/ult/"
+# testingfilename = "19-PSA.usfm"
 
-filesLocation = "C:/Users/benja/Downloads/ult/*.usfm"
+def turnUSFMintoTxt(filesLocation, uSLt):
 
-bibleAsList = []
+    #filesLocation = "C:/Users/benja/Downloads/ust/*.usfm"
 
-for filename in glob.glob(filesLocation):
+    bibleAsList = []
 
-    with io.open(filename, encoding='utf8') as f:
-        bookLines = f.readlines()
+    for filename in glob.glob(filesLocation):
 
-    chapter = ''
-    paragraph = True
-    book = ''
-    verseNumber = ''
-    newLine = ''
-    newChapter = True
-    title = ''
-    titleLine = False
+        with io.open(filename, encoding='utf8') as f:
+            bookLines = f.readlines()
 
-    for line in bookLines:
-        if line.startswith('\\v'):
-            if not newChapter:
-                bibleAsList.append(newLine)
-                newLine = ''            
-            verseNumber = re.split('\s', line)[1]
-            newLine += verseNumber
-            if len(title) > 0:
-                newLine += title
-                title = ''
-            if paragraph:
-                if not titleLine:
-                    newLine += ' ¶'
-                else: 
-                    newLine += ' <br>'
-                    titleLine = False
-                paragraph = False
-            verseContent = re.split('\s', line, 2)[2]
-            verseContent = verseContent.replace('\\sp', '<br>')
-            verseContent = verseContent.replace('\\qs*', '')
-            verseContent = verseContent.replace('\\qs', ' ')
-            verseContent = verseContent.replace('\\q1', '<br>')
-            verseContent = verseContent.replace('\\q2', '<br>')
-            verseContent = verseContent.replace('\\q', '<br>')
-            newLine += " " + verseContent
-            if newChapter:
-                bibleAsList.append(newLine)
-                newLine = '' 
-                newChapter = False
-                
-        elif line.startswith('\\p') or line.startswith('\\q') or line.startswith(r'\\q\d'):
-            paragraph = True
+        chapter = ''
+        paragraph = True
+        book = ''
+        verseNumber = ''
+        newLine = ''
+        newChapter = True
+        title = ''
+        titleLine = False
 
-        elif line.startswith('\\d'): # include Psalm titles
-            title = ' ¶ ' + re.split('\s', line, 1)[1].strip()
-            titleLine = True
-        elif re.split('\s', line)[0] == '\\c':
-            chapter = re.split('\s', line)[1]
-            newChapter = True
-            newLine += chapter + ":"
-        elif line.startswith('\\h'):
-            book = re.split('\s', line, 1)[1]
-            if book.startswith("Song"): book = "Song"
-            if book[0] == '1' or book[0] == '2' or book[0] == '3':
-                book = book[0] + book[2:]
-            newLine += book.strip() + ' '
-    
+        for line in bookLines:
+            if line.startswith('\\v'):
+                if not newChapter:
+                    bibleAsList.append(newLine)
+                    newLine = ''            
+                verseNumber = re.split('\s', line)[1]
+                newLine += verseNumber
+                if len(title) > 0:
+                    newLine += title
+                    title = ''
+                if paragraph:
+                    if not titleLine:
+                        newLine += ' ¶'
+                    else: 
+                        newLine += ' <br>'
+                        titleLine = False
+                    paragraph = False
+                verseContent = re.split('\s', line, 2)[2]
+                verseContent = verseContent.replace('\\sp', '<br>')
+                verseContent = verseContent.replace('\\qs*', '')
+                verseContent = verseContent.replace('\\qs', ' ')
+                verseContent = verseContent.replace('\\q1', '<br>')
+                verseContent = verseContent.replace('\\q2', '<br>')
+                verseContent = verseContent.replace('\\q', '<br>')
+                verseContent = verseContent.replace('\\p', '<br>')
+                verseContent = verseContent.replace('\\li', '<br>')
+                verseContent = verseContent.replace('\\m', '<br>')
+                verseContent = verseContent.replace('\\f +', '<i><b>fn:</b> ')
+                verseContent = verseContent.replace('\\f*', '</i>')               
+                newLine += " " + verseContent
+                if newChapter:
+                    bibleAsList.append(newLine)
+                    newLine = '' 
+                    newChapter = False
+                    
+            elif line.startswith('\\p') or line.startswith('\\q') or line.startswith(r'\\q\d'):
+                paragraph = True
 
-with io.open('Bible for Accordance.txt', 'w', encoding='utf-8')as f:
-    f.writelines(bibleAsList)
+            elif line.startswith('\\d'): # include Psalm titles
+                title = ' ¶ ' + re.split('\s', line, 1)[1].strip()
+                titleLine = True
+            elif re.split('\s', line)[0] == '\\c':
+                chapter = re.split('\s', line)[1]
+                newChapter = True
+                newLine += chapter + ":"
+            elif line.startswith('\\h'):
+                book = re.split('\s', line, 1)[1]
+                if book.startswith("Song"): book = "Song"
+                if book[0] == '1' or book[0] == '2' or book[0] == '3':
+                    book = book[0] + book[2:]
+                newLine += book.strip() + ' '
+        
 
-"""
-for item in bookAsList:
-    print(item)
+    with io.open("C:/Users/benja/Downloads/" + uSLt + ' for Accordance.txt', 'w', encoding='utf-8')as f:
+        f.writelines(bibleAsList)
 
-"""
+turnUSFMintoTxt("C:/Users/benja/Downloads/ust/*.usfm", "UST")
+turnUSFMintoTxt("C:/Users/benja/Downloads/ult/*.usfm", "ULT")
